b8b2dcf52cc8ed98319a034444536513533b9e8e
mxinseo <kyungminseo10@gmail.com>
2025-07-23 11:50:03 +0900

    Feat : 지은 s3_utils.py 가져오기

---
+++ s3_utils.py
@@ -0,0 +1,39 @@
+import boto3
+import requests
+import uuid
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_REGION     = os.getenv('AWS_REGION')
+BUCKET_NAME    = os.getenv('BUCKET_NAME')
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id     = AWS_ACCESS_KEY,
+    aws_secret_access_key = AWS_SECRET_KEY,
+    region_name           = AWS_REGION
+)
+
+def upload_video(local_path, s3_folder='videos'):
+    filename = os.path.basename(local_path)
+    key = f"{s3_folder}/{uuid.uuid4().hex}_{filename}"
+    s3.upload_file(local_path, BUCKET_NAME, key)
+    url = f"https://{BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{key}"
+    return url
+
+SPRING_URL = 'http://localhost:8080/api/v1/videos'  #엔드포인트
+
+def send_url(video_url, fall_detected):
+    payload = {
+        'videoUrl': video_url,
+        'fallDetected': fall_detected  # 또는 'fall_detected'로도 가능 (서버 쪽 JSON 필드 이름에 맞춰야 함)
+    }
+    resp = requests.post(SPRING_URL, json=payload)
+    if resp.status_code == 200:
+        print(f"[Spring] 저장 성공: {video_url}")
+    else:
+        print(f"[Spring] 저장 실패 ({resp.status_code}): {resp.text}")
